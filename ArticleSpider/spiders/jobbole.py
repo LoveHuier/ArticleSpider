522e345ca55e7bb49189628833db230737019f00
yanping.gao <yanping.gao@jrdcom.com>
2017-08-25 14:42:39 +0800

    get title and create_data by xpath

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -5,7 +5,10 @@ import scrapy
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/112265/']
 
     def parse(self, response):
+        title = response.xpath('//*[@id="post-112265"]/div[1]/h1/text()').extract()[0].strip()
+        create_data = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].strip().replace(
+            " ·", "")
         pass
